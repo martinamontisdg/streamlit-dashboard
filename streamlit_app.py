311e4546aa04dbd7712547ab48ad076e61fb3e48
martinamontisdg <martina.monti@sdggroup.com>
2025-06-05 16:25:47 +0200

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ conn = snowflake.connector.connect(
     schema=st.secrets["SNOWFLAKE_SCHEMA"],
 )
 
-query = "SELECT * FROM MM_DWF_ORDERS"
+query = "SELECT * FROM TRAINIG_FASTSHIP.DWH.MM_DWF_ORDERS"
 df = pd.read_sql(query, conn)
 
 conn.close()
